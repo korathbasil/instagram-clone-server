b90318ff0ac6285dceef010e4a369b3a8b5457ed
Bazil Korath <korathbasil@live.com>
2021-08-02 21:31:00 +0530

    add user service and controller files

---
+++ src/REST/controllers/userController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+import { userService } from "../services";
+
+export default {
+  userSignup: (req: Request, res: Response) => {},
+};
--- src/REST/routes/userRoute.ts
+++ src/REST/routes/userRoute.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 
+import { userController } from "../controllers";
+
 const router = Router();
 
-router.post("/");
+router.post("/signup");
 
 export default router;
