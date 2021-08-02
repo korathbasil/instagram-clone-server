dd832aab9c71a3d0d891d197d1dd3aa7a6e7b292
Bazil Korath <korathbasil@live.com>
2021-08-02 19:30:29 +0530

    feat: add dotenv config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import ExpressApp from "./app";
 
+import { NODE_ENV } from "./config/constants";
+import env from "dotenv";
+
+if (NODE_ENV === "dev" || NODE_ENV === "test") {
+  env.config();
+}
+
 const startServer = () => {
   ExpressApp.listen(3000, () => {
     console.info("Server started at PORT: " + 3000);
