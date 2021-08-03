ee2ea2c031cf9dd1edc7579ca7627c4983ce452f
Bazil Korath <korathbasil@live.com>
2021-08-03 15:37:59 +0530

    add Post model

--- src/api/models/PostModel.ts
+++ src/api/models/PostModel.ts
@@ -1,1 +1,38 @@
-export default {};
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("posts")
+class Post extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: "varchar",
+    length: 300,
+    default: "",
+  })
+  caption: string;
+
+  @Column()
+  image: string;
+
+  @Column({ default: 0 })
+  likes_count: number;
+
+  @Column({ default: 0 })
+  comments_count: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Post;
