1ffb67a21b3089c432f1d4c474d08d0e3e7cb319
Bazil Korath <korathbasil@live.com>
2021-08-03 21:48:56 +0530

    add express-fileupload module
    add fileupload middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import fileUpload from "express-fileupload";
 
 import { userRoute, postRoute } from "./api/routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(fileUpload());
 
 app.use("/api/user", userRoute);
 app.use("/api/post", postRoute);
