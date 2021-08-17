35ec222e78d871471cfa1d157e343e8dc1bf13e4
Bazil Korath <korathbasil@live.com>
2021-08-17 21:16:01 +0530

    feat: add client connection from dev server
    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Response } from "express";
+import cors from "cors";
 import multer from "multer";
 
+import { CLIENT_URL } from "./config/constants";
 import { userRoute, postRoute } from "./api/routes";
 
 const app = express();
 
+app.use(cors({ origin: CLIENT_URL }));
 app.use(express.static(__dirname + "/public"));
 
 app.use(express.json());
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,6 +2,8 @@ export const NODE_ENV = process.env.NODE_ENV || "dev";
 
 export const PORT = process.env.PORT || 8000;
 
+export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
+
 export const DB_PASSWORD = process.env.DB_PASSWORD || undefined;
 
 export const JWT_SECRET = process.env.JWT_SECRET || "jsonwebtokensecret";
