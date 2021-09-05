367493a1173acd30b198aaca55930e086c900daa
Bazil Korath <korathbasil@live.com>
2021-09-05 19:45:58 +0530

    add file controller

--- src/api/controllers/index.ts
+++ src/api/controllers/index.ts
@@ -1,4 +1,5 @@
 import userController from "./userController";
 import postController from "./postController";
+import fileController from "./fileController";
 
-export { userController, postController };
+export { userController, postController, fileController };
--- src/api/routes/fileRoute.ts
+++ src/api/routes/fileRoute.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 
+import { fileController } from "../controllers";
+
 const router = Router();
 
-router.get("/image/:id");
+router.get("/image/:id", fileController.getImage);
 
 export default router;
