780e460482716f51d819400568b6327675c03f51
Bazil Korath <korathbasil@live.com>
2021-12-31 00:29:38 +0530

    add cleanup db in test setup

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,7 @@
 import { getConnection } from "typeorm";
 
 import connectToDatabase from "../config/db";
+import { User, Chat, UserChat } from "../api/models";
 
 before(async () => {
   await connectToDatabase((err) => {
@@ -9,19 +10,30 @@ before(async () => {
 });
 
 beforeEach(async () => {
-  const connection = getConnection();
-
-  const entities = connection.entityMetadatas;
-
-  for (const entity of entities) {
-    const repository = connection.getRepository(entity.name); // Get repository
-    await repository.clear(); // Clear each entity table's content
-  }
+  await clearDatabase();
 });
 
 afterEach(async () => {});
 
 after(async () => {
+  await clearDatabase();
+  await closeDatabase();
+});
+
+async function clearDatabase() {
+  // const connection = getConnection();
+  // const entities = connection.entityMetadatas;
+  // for (const entity of entities) {
+  //   const repository = connection.getRepository(entity.name); // Get repository
+  //   await repository.delete({}); // Clear each entity table's content
+  // }
+
+  await UserChat.delete({});
+  await Chat.delete({});
+  await User.delete({});
+}
+
+async function closeDatabase() {
   const connection = getConnection();
   await connection.close();
-});
+}
