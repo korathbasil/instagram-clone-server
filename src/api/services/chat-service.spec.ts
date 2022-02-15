f3482bd2eea587b24c9e6771c0ddc6fcc8e0e205
Bazil Korath <korathbasil@live.com>
2022-02-16 01:03:55 +0530

    edit

--- src/api/services/chat-service.spec.ts
+++ src/api/services/chat-service.spec.ts
@@ -3,29 +3,27 @@ import { ChatService } from "./chat-service";
 import { User } from "../models";
 
 describe("Chat Service", () => {
+  const user1details = {
+    name: "James Bond",
+    email: "james@email.com",
+    username: "jamesbond007",
+    password: "iamjamesbond",
+  };
+  const user2details = {
+    name: "John Cena",
+    email: "john@email.com",
+    username: "johncena",
+    password: "iamjohncena",
+  };
   describe("createChat", () => {
     it("creates a chat", async () => {
-      const user1details = {
-        name: "James Bond",
-        email: "james@email.com",
-        username: "jamesbond007",
-        password: "iamjamesbond",
-      };
-      const user2details = {
-        name: "John Cena",
-        email: "john@email.com",
-        username: "johncena",
-        password: "iamjohncena",
-      };
-
       const user1 = User.create(user1details);
       await user1.save();
 
       const user2 = await User.create(user2details);
       await user2.save();
 
-      const chat = await ChatService.createChat(user1.id);
-      console.log(chat);
+      const chat = await ChatService.createChat(user1.id, user2.id);
 
       expect(typeof chat).equal("object");
     });
--- src/api/services/chat-service.ts
+++ src/api/services/chat-service.ts
@@ -1,11 +1,17 @@
 import { Chat, UserChat } from "../models";
 
 export class ChatService {
-  public static async createChat(userId: number) {
+  public static async createChat(userId: number, secondUserId: number) {
     const chat = Chat.create({ name: "Hello" });
     await chat.save();
 
     const userChat = UserChat.create({ chat_id: chat.id, user_id: userId });
-    return userChat.save();
+    await userChat.save();
+
+    const secondUserChat = UserChat.create({
+      chat_id: userChat.chat_id,
+      user_id: secondUserId,
+    });
+    await secondUserChat.save();
   }
 }
